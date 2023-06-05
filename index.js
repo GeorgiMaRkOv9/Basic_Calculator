1dc8ebf98c24420f27f5fbed8ebdb41250892701
Georgi Markov <georgi.markov9@googlemail.com>
2023-06-05 02:28:23 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const butttonsElement = document.querySelectorAll("button");
+
+const inputFieldElement = document.getElementById("result");
+
+for (let i = 0; i < butttonsElement.length; i++) {
+    butttonsElement[i].addEventListener("click", () => {
+        const buttonValue = butttonsElement[i].textContent;
+        if (buttonValue === "C") {
+            clearResult();
+        } else if (buttonValue === "=") {
+            calculateResult();
+        } else {
+            appendValue(buttonValue);
+        }
+    });
+}
+
+function clearResult() {
+    inputFieldElement.value = "";
+}
+
+function calculateResult() {
+    inputFieldElement.value = eval(inputFieldElement.value)
+}
+
+function appendValue(buttonValue) {
+    inputFieldElement.value += buttonValue;
+}
